56fdd636af75384d434368fb98c74a4d4c1838b2
PipitPitriYani07 <fpipit271@gmail.com>
2024-05-09 14:42:58 +0700

    feature penambahan model baru dan perbaikan model user

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use App\Models\DetailTransaksi;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+      'users_id',
+      'tgl_pinjam',
+      'tgl_kembali'
+
+    ];
+    protected $table = 'transaksi';
+
+    //relasi ke tabel detail transaksi
+    public function detailtransaksi(){
+        return $this->hasMany(DetailTransaksi:: class, 'transaksi_id', 'id');
+    }
+    public function user(){
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
+
+}
+
